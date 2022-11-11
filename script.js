470b4e938afe6afbd00330f0b6f4b8b0cb39220b
Brambs <gabriel128.br@gmail.com>
2022-11-11 06:38:18 -0300

    update

---
+++ script.js
@@ -0,0 +1,30 @@
+let numero = 3455.949
+let word = `Nicolau`
+let phrase = `I'm going to latinoware today after lunch`
+let array_top = phrase.split(' ').join('_').toUpperCase()
+let array = ['html', 'javascript', 'css', 'react']
+let palavra = typeof word === 'string' ? 'É uma String' : 'Não é uma String'
+
+console.log(palavra)
+// array.push('scss', 'flutter')
+// array.unshift('scratch')
+
+// console.log('Array Original')
+// console.log(array)
+// console.log('Splice')
+// console.log(array.splice(1, 2))
+// console.log('Slice')
+// console.log(array.slice(2, 5))
+// console.log(
+//   '--------------------------------------------------------------------'
+// )
+// array.pop()
+// array.shift()
+// console.log(array)
+
+// console.log(Array.from(word))
+// // console.log(phrase.includes(' going to'))
+// console.log(phrase)
+// console.log(array_top)
+// console.log(word.toUpperCase())
+// console.log(number.toFixed(2).replace('.', ','))
